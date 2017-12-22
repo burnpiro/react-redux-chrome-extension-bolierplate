48201550ba12378aa19a56d533cdfc5ba0c57530
Kemal Erdem <kemalpiro@gmail.com>
2017-12-22 21:18:44 +0000

    enable copying icon file when in dev build

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -260,7 +260,10 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new CopyWebpackPlugin([{ from: 'public/manifest.json', to: ''}]),
+    new CopyWebpackPlugin([
+      { from: 'public/manifest.json', to: ''},
+      { from: 'public/icon.png', to: ''}
+    ]),
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
